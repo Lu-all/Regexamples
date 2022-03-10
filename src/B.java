9c9476d94241db7b8162beb811163f3821eb4726
Alfonsozm <83079664+Alfonsozm@users.noreply.github.com>
2022-03-10 18:23:26 +0100

    update B

--- src/B.java
+++ src/B.java
@@ -6,10 +6,10 @@ public class B {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String text = s.nextLine();
-        Pattern p = Pattern.compile("(E?(-|\\s)?(\\d\\d\\d\\d)(-|\\s)?)([A-Z]{3})");
+        Pattern p = Pattern.compile("\\b(E[- ]?)?(\\d{4})[- ]?([A-Z]{3})\\b");
         Matcher m = p.matcher(text);
         while(m.find()){
-            System.out.println(m.group());
+            System.out.println(m.group(0));
         }
     }
 }
