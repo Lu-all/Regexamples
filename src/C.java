92b8515bf5d3d0a624788ab1461c5deaaa42d6ac
Alfonsozm <83079664+Alfonsozm@users.noreply.github.com>
2022-03-10 22:47:50 +0100

    update C not working

--- src/C.java
+++ src/C.java
@@ -6,10 +6,14 @@ public class C {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String text = s.nextLine();
-        Pattern p = Pattern.compile("([a-z]{1}\\.[a-z]{2,}\\.\\d{4}@alumnos\\.urjc\\.es)|([a-z]+\\.[a-z]+@urjc\\.es)");
+        Pattern p = Pattern.compile("(?:[a-z]\\.([a-z]{2,})\\.(\\d{4})@alumnos\\.urjc\\.es)|(?:([a-z]+)\\.([a-z]+)@urjc\\.es)");
         Matcher m = p.matcher(text);
-        while(m.find()){
-            System.out.println(m.group());
+        while (m.find()) {
+            if (m.group(1) != null && !m.group(1).isEmpty()) {
+                System.out.println("alumno " + m.group(1)+ " matriculado en " + m.group(2));
+            } else {
+                System.out.println("profesor " + m.group(3) + " " + m.group(4));
+            }
         }
     }
 }
