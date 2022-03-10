e565de934166d9ea18e8bebe3f096fe5b4d51e45
Alfonsozm <83079664+Alfonsozm@users.noreply.github.com>
2022-03-10 17:49:22 +0100

    first A

--- src/A.java
+++ src/A.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class A {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String text = s.nextLine();
+        Pattern p = Pattern.compile("\\w+");
+        Matcher m = p.matcher(text);
+        while(m.find()){
+            System.out.println(m.group());
+        }
+    }
 }
