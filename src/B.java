d3c2430e886c6cd3097140af20f67f1379e92b79
Luciana-fa <cati23816@gmail.com>
2022-03-10 17:51:52 +0100

    First two regex

--- src/B.java
+++ src/B.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class A {
+public class B {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String text = s.nextLine();
-        Pattern p = Pattern.compile("([0-9]{4})");
+        Pattern p = Pattern.compile("(E?(-|\\s)?(\\d\\d\\d\\d)(-|\\s)?)([A-Z]{3})");
         Matcher m = p.matcher(text);
         while(m.find()){
             System.out.println(m.group());
