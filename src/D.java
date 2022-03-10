58555fb0432b89dca65ce8e5e61511f40c46b98b
Luciana-fa <cati23816@gmail.com>
2022-03-10 18:20:47 +0100

    Fourth regex improved

--- src/D.java
+++ src/D.java
@@ -6,10 +6,10 @@ public class D {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String text = s.nextLine();
-        Pattern p = Pattern.compile("C[^ ]* (\\w+)\\s(?:[^\\d]*)?(\\d+), (\\d+)");
+        Pattern p = Pattern.compile("(?:(?:C\\/)|(?:Calle)) ([A-Z]\\w+),?\\s*(?:(?:Nº)|(?:N *)|(?:Nº *)|(?:n))?(\\d+), *(\\d{5})");
         Matcher m = p.matcher(text);
         while(m.find()){
-            System.out.println(m.group());
+            System.out.println(m.group(3)+"-"+m.group(1)+"-"+m.group(2));
         }
     }
 }
