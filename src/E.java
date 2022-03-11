d01e43515107afe8fbcd9b1636384de180fcb7ec
Alfonsozm <83079664+Alfonsozm@users.noreply.github.com>
2022-03-11 18:03:39 +0100

    first try to E

---
+++ src/E.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class E {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Pattern p = Pattern.compile("\\d\\d:\\d\\d:\\d\\d\\.\\d\\d\\d ([A-Z]+) \\d+\\s*-\\s\\[([a-z]+)\\] (?:\\w+\\.)+([a-zA-z]+) : (.+)");
+        while(s.hasNextLine()) {
+            String text = s.nextLine();
+            Matcher m = p.matcher(text);
+            while (m.find()) {
+                System.out.println("\"" + m.group(1) + "\",\"" + m.group(2) + "\",\"" + m.group(3) + "\",\"" + m.group(4) + "\"");
+            }
+        }
+    }
+}
