e42e9f6b9e092d733610d3fa931300945605945a
Luciana-fa <cati23816@gmail.com>
2022-03-17 17:25:03 +0100

    E4 working

--- src/D.java
+++ src/D.java
@@ -6,7 +6,7 @@ public class D {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String text = s.nextLine();
-        Pattern p = Pattern.compile("(?:(?:C\\/)|(?:Calle)) ([A-ZÑ][\\wñ]*),? *(?:[Nn]º? ?)?(\\d+), *(\\d{5})");
+        Pattern p = Pattern.compile("(?:(?:C\\/)|(?:Calle)) ([A-ZÑÀ-ÿ][\\wñÀ-ÿ]*),? *(?:[Nn]º? ?)?(\\d+), *(\\d{5})");
         Matcher m = p.matcher(text);
         while(m.find()){
             System.out.println(m.group(3)+"-"+m.group(1)+"-"+m.group(2));
