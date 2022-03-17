07c0c540b9d4645d3ac37e8e926136166603f129
Alfonsozm <83079664+Alfonsozm@users.noreply.github.com>
2022-03-17 17:49:58 +0100

    update to E

--- src/E.java
+++ src/E.java
@@ -5,13 +5,11 @@ import java.util.regex.Pattern;
 public class E {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        Pattern p = Pattern.compile("\\d\\d:\\d\\d:\\d\\d\\.\\d\\d\\d ([A-Z]+) \\d+ +-+ +\\[([a-z]+)\\] (?:\\w+\\.)*([a-zA-z]+) : (.+)");
-        while(s.hasNextLine()) {
-            String text = s.nextLine();
-            Matcher m = p.matcher(text);
-            while (m.find()) {
-                System.out.println("\"" + m.group(1) + "\",\"" + m.group(2) + "\",\"" + m.group(3) + "\",\"" + m.group(4) + "\"");
-            }
+        String text = s.nextLine();
+        Pattern p = Pattern.compile("([A-Z]+)(?:.*)\\[(\\w+)](?:[\\sa-z.]*)([A-Z][\\w]+)(?:\\s*:\\s*)(.+)");
+        Matcher m = p.matcher(text);
+        while(m.find()){
+            System.out.println("\""+m.group(1)+"\",\""+m.group(2)+"\",\""+m.group(3)+"\",\""+m.group(4)+"\"");
         }
     }
 }
