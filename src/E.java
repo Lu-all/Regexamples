7017551d7fb4532ac2efbd536afa8ace872ec913
Luciana-fa <cati23816@gmail.com>
2022-03-17 17:57:27 +0100

    + for * in Package group

--- src/E.java
+++ src/E.java
@@ -6,7 +6,7 @@ public class E {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String text = s.nextLine();
-        Pattern p = Pattern.compile("([A-Z]+).*\\[(\\w+)][\\sa-z.]*([A-Z][\\w]+)\\s*:\\s*(.+)");
+        Pattern p = Pattern.compile("([A-Z]+).*\\[(\\w+)][\\sa-z.]*([A-Z][\\w]*)\\s*:\\s*(.+)");
         Matcher m = p.matcher(text);
         while(m.find()){
             System.out.println("\""+m.group(1)+"\",\""+m.group(2)+"\",\""+m.group(3)+"\",\""+m.group(4)+"\"");
